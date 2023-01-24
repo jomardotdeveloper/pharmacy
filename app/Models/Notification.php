949b0c5c6eb655213568b5f48bd26d3cc69a70a3
jomardotdeveloper <jomar.developer@gmail.com>
2023-01-24 15:32:44 +0800

    TODAY

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -31,7 +31,28 @@ class Notification extends Model
         if($mins < 60) {
             return $mins . " mins ago";
         }else{
-            return round(intval(abs($to_time - $from_time) / 60 / 60, 2)) . " hours ago";
+            $hours = round(intval(abs($to_time - $from_time) / 60 / 60, 2)) . " hours ago";
+            if($hours < 24) {
+                return $hours;
+            }else{
+                $days = round(intval(abs($to_time - $from_time) / 60 / 60 / 24, 2)) . " days ago";
+                if($days < 7) {
+                    return $days;
+                }else{
+                    $weeks = round(intval(abs($to_time - $from_time) / 60 / 60 / 24 / 7, 2)) . " weeks ago";
+                    if($weeks < 4) {
+                        return $weeks;
+                    }else{
+                        $months = round(intval(abs($to_time - $from_time) / 60 / 60 / 24 / 7 / 4, 2)) . " months ago";
+                        if($months < 12) {
+                            return $months;
+                        }else{
+                            $years = round(intval(abs($to_time - $from_time) / 60 / 60 / 24 / 7 / 4 / 12, 2)) . " years ago";
+                            return $years;
+                        }
+                    }
+                }
+            }
         }
         return round(intval(abs($to_time - $from_time) / 60, 2)) . " mins ago";
     }
